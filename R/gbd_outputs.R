724991e03268bca8dd99fcb5335e7b44af71facb
jeffeaton <jeffrey.eaton@imperial.ac.uk>
2018-12-19 17:17:14 -0800

    add get_gbd_outputs() function

--- R/gbd_outputs.R
+++ R/gbd_outputs.R
@@ -10,95 +10,85 @@
 #'
 #' pjnz <- system.file("extdata/testpjnz", "Botswana2018.PJNZ", package="eppasm")
 #' fp <- prepare_directincid(pjnz)
-#' mod <- simmod(fp)
+#' mod <- simmod(fp, "R")
+#'
+#' gbdout <- get_gbd_outputs(mod, fp)
+#' write.csv(gbdout, "gbd-output-run1.csv", row.names=FALSE)
 #'
 #' @export
 
 get_gbd_outputs <- function(mod, fp) {
 
-  years <- get_proj_years(fp$ss)
-
-  dim(attr(mod, "hivdeaths"))
-
-}
-
-#' Get vector of years spanned by projections
-get_proj_years <- function(ss){
-  ss$proj_start + 1:ss$PROJ_YEARS - 1L
-}
-
-
-#' Add dimnames to spec model output
-#'
-#' `mod_dimnames` assigns dimnames to spec model outputs.
-#'
-#' @param mod EPP-ASM model output of class `spec`
-#' @param ss Model state space inputs
-#'
-#' @return EPP-ASM model output with labelled dimensions
-#'
-#' @examples
-#' pjnz <- system.file("extdata/testpjnz", "Botswana2018.PJNZ", package="eppasm")
-#' fp <- prepare_directincid(pjnz)
-#' mod <- simmod(fp)
-#'
-#' 
-#'
-#' @export
-mod_dimnames <- function(mod, ss){
-
-  yrlbl <- get_proj_years(ss)
-  sexlbl <- c("male", "female")
-  hAGlbl <- c("15-16", "17-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50+")
-  pAGlbl <- 15:80
-  pDSlbl <- c("hivn", "hivp")
-  hDSlbl <- c(">500", "350-499", "250-349", "200-249", "100-199", "50-99", "<50")
-  hTSlbl <- c("art0mos", "art6mos", "art1yr")
-
-  dimnames(mod) <- list(age = pAGlbl, sex = sexlbl, hiv = pDSlbl, year = yrlbl)
-  dimnames(attr(mod, "hivpop")) <- list(cd4stage = hDSlbl, agegr = hAGlbl, sex = sexlbl, year = yrlbl)
-  dimnames(attr(mod, "artpop")) <- list(artdur = hTSlbl, cd4stage = hDSlbl, agegr = hAGlbl, sex = sexlbl, year = yrlbl)
-
-  dimnames(attr(mod, "infections")) <- dimnames(mod)
-  dimnames(attr(mod, "hivdeaths")) <- dimnames(mod)
-  dimnames(attr(mod, "natdeaths")) <- dimnames(mod)
-  dimnames(attr(mod, "prev15to49")) <- yrlbl
-  dimnames(attr(mod, "pregprev")) <- yrlbl
-  dimnames(attr(mod, "incid15to49")) <- yrlbl
-
-  mod
-}
-
-#' Convert aggregate HIV population to single year
-#'
-#' EPP-ASM tracks the CD4 distribution and ART duration
-#' of the HIV population by coarse age groups for
-#' computational efficiency. The model assumes that
-#'
-#' @param mod 
-#'
-#' 
-#' @export
-hivpop_singleyear <- function(mod, ss){
-
-  hivp <- mod[ , , ss$hivn.idx, ]
-  hivpop <- attr(mod, "hivpop")
-  artpop <- attr(mod, "artpop")
-
-  denom <- colSums(hivpop) + colSums(artpop,,2)
-  hivdist <- sweep(hivpop, 2:4, denom, "/")
-  artdist <- sweep(artpop, 3:5, denom, "/")
-
-  hivdist[is.na(hivdist)] <- 0
-  artdist[is.na(artdist)] <- 0
-
-  hivpop1 <- sweep(hivdist[,ss$ag.idx,,], 2:4, hivp, "*")
-  artpop1 <- sweep(artdist[,,ss$ag.idx,,], 3:5, hivp, "*")
-
-  dimnames(hivpop1)[2:4] <- dimnames(hivp)
-  dimnames(artpop1)[3:5] <- dimnames(hivp)
-
-  list(hivpop1 = hivpop1, artpop1 = artpop1)
-}
+  mod <- mod_dimnames(mod, fp$ss)
+  hp1 <- hivpop_singleage(mod, fp$ss)
   
-  v <- read.csv("~/Downloads/spec_example.csv")
+  pop <- as.data.frame.table(apply(mod, c(1, 2, 4), sum), responseName = "pop")
+  hiv_deaths <- as.data.frame.table(attr(mod, "hivdeaths"),
+                                    responseName = "hiv_deaths")
+  non_hiv_deaths <- as.data.frame.table(attr(mod, "natdeaths"),
+                                        responseName = "non_hiv_deaths")
+  new_hiv <- as.data.frame.table(attr(mod, "infections"),
+                                 responseName = "new_hiv")
+  pop_neg <- as.data.frame.table(mod[,,fp$ss$hivn.idx,])
+  
+  total_births <- as.data.frame.table(get_births(mod, fp), responseName = "total_births")
+  total_births$sex <- "female"
+  pregprev <- as.data.frame.table(get_pregprev(mod, fp, hp1), responseName = "pregprev")
+  pregprev$sex <- "female"
+  
+  pop_art <- as.data.frame.table(colSums(hp1$artpop1,,2), responseName = "pop_art")
+
+  hivpop_daly <- as.data.frame.table(get_daly_hivpop(hp1$hivpop), responseName = "value")
+  hivpop_daly <- data.table::dcast(hivpop_daly, ... ~ cd4daly)
+
+  v <- pop
+  v <- merge(v, hiv_deaths, all.x=TRUE)
+  v <- merge(v, non_hiv_deaths, all.x=TRUE)
+  v <- merge(v, new_hiv, all.x=TRUE)
+  v <- merge(v, pop_neg, all.x=TRUE)
+  v <- merge(v, total_births, all.x=TRUE)
+  v <- merge(v, pregprev, all.x=TRUE)
+  v$hiv_births <- v$total_births * v$pregprev  # number of births to HIV positive women
+  v$birth_prev <- NA                           # HIV prevalence among newborns -- not yet output
+  v <- merge(v, pop_art, all.x=TRUE)
+  v <- merge(v, hivpop_daly, all.x=TRUE)
+
+  v
+}
+
+
+#' Births by single age
+#' 
+get_births <- function(mod, fp){
+
+  py <- fp$ss$PROJ_YEARS
+  fertpop <- apply(mod[fp$ss$p.fert.idx, fp$ss$f.idx, , ] +
+                   mod[fp$ss$p.fert.idx, fp$ss$f.idx, , c(1, 1:(py-1))],
+                   c(1, 3), sum) / 2
+  fertpop * fp$asfr
+}
+
+
+#' HIV prevalence among pregant women by single age
+#' 
+get_pregprev <- function(mod, fp, hp1){
+
+  ss <- fp$ss
+  py <- fp$ss$PROJ_YEARS
+  expand_idx <- rep(fp$ss$h.fert.idx, fp$ss$h.ag.span[fp$ss$h.fert.idx])
+  hivn_w <- mod[ss$p.fert.idx, ss$f.idx, ss$hivn.idx,  ]
+  hivp_w <- colSums(hp1$hivpop[ , ss$p.fert.idx, ss$f.idx, ] * fp$frr_cd4[ , expand_idx, ])
+  art_w <- colSums(hp1$artpop[ , , ss$p.fert.idx, ss$f.idx, ] * fp$frr_art[ , , expand_idx, ],,2)
+  denom_w <- hivn_w + hivp_w + art_w
+  pregprev_a <- 1 - (hivn_w + hivn_w[ , c(1, 1:(py-1))]) / (denom_w + denom_w[ , c(1, 1:(py-1))])
+
+  pregprev_a
+}
+
+
+get_daly_hivpop <- function(hivpop1){
+  idx <- rep(c("pop_gt350", "pop200to350", "pop_lt200"), c(2, 2, 3))
+  v <- apply(hivpop1, 2:4, fastmatch::ctapply, idx, sum)
+  names(dimnames(v))[1] <- "cd4daly"
+  v
+}
